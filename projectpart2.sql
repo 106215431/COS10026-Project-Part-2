b666220c09fdcfc645b11e6d161e4ce27c733d07
106215431 <106215431@student.swin.edu.au>
2025-11-01 23:19:38 +0700

    New database fieldnames and default eoi entries

--- projectpart2.sql
+++ projectpart2.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 01, 2025 at 04:05 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 01, 2025 lúc 05:18 PM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,52 +18,64 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `projectpart2`
+-- Cơ sở dữ liệu: `projectpart2`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `eoi`
+-- Cấu trúc bảng cho bảng `eoi`
 --
 
 CREATE TABLE `eoi` (
   `EOInumber` int(11) NOT NULL,
-  `job_reference_number` varchar(20) DEFAULT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
-  `street_address` varchar(100) DEFAULT NULL,
-  `suburb` varchar(50) DEFAULT NULL,
-  `state` varchar(3) DEFAULT NULL,
-  `postcode` varchar(4) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
-  `phone` varchar(20) DEFAULT NULL,
+  `jobRef` varchar(20) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `dob` date NOT NULL,
+  `gender` enum('Male','Female','Other') NOT NULL,
+  `streetAddress` varchar(40) NOT NULL,
+  `suburb` varchar(40) NOT NULL,
+  `state` enum('VIC','NSW','QLD','NT','WA','SA','TAS','ACT') NOT NULL,
+  `postcode` char(4) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `phone` varchar(12) NOT NULL,
   `skill1` varchar(50) DEFAULT NULL,
   `skill2` varchar(50) DEFAULT NULL,
   `skill3` varchar(50) DEFAULT NULL,
-  `other_skills` text DEFAULT NULL,
-  `status` varchar(10) NOT NULL DEFAULT 'New'
+  `skill4` varchar(50) DEFAULT NULL,
+  `otherSkills` varchar(200) DEFAULT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'New'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indexes for dumped tables
+-- Đang đổ dữ liệu cho bảng `eoi`
+--
+
+INSERT INTO `eoi` (`EOInumber`, `jobRef`, `firstName`, `lastName`, `dob`, `gender`, `streetAddress`, `suburb`, `state`, `postcode`, `email`, `phone`, `skill1`, `skill2`, `skill3`, `skill4`, `otherSkills`, `status`) VALUES
+(1, 'CE-VIC-1025', 'sdssdsd', 'dsdsds', '2002-12-12', 'Female', 'dssddad', 'sadsadad', 'VIC', '3008', 'hienvgh@gmail.com', '904771558', 'Networking', 'Cybersecurity', NULL, NULL, 'dsdsdsdsds', 'New'),
+(2, 'PE-QLD-1243', 'sdss', 'dsdsdsd', '2002-12-13', 'Other', '123 titan street', 'dsdsdsd', 'ACT', '0038', 'haireif@gmail.com', '904771558', 'Programming', NULL, NULL, NULL, '', 'New'),
+(3, 'CC-TAS-773', 'Vu', 'Vui', '2888-03-10', 'Male', '67 sigma street', 'what town', 'SA', '5111', 'hairyeif@gmail.com', '969767065', NULL, NULL, NULL, NULL, 'uh i have uh gaming laptop pls accept', 'New');
+
+--
+-- Chỉ mục cho các bảng đã đổ
 --
 
 --
--- Indexes for table `eoi`
+-- Chỉ mục cho bảng `eoi`
 --
 ALTER TABLE `eoi`
   ADD PRIMARY KEY (`EOInumber`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT cho các bảng đã đổ
 --
 
 --
--- AUTO_INCREMENT for table `eoi`
+-- AUTO_INCREMENT cho bảng `eoi`
 --
 ALTER TABLE `eoi`
-  MODIFY `EOInumber` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `EOInumber` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
