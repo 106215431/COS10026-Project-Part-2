63c9735867000ffac0a6c7431161cde5dc2b6719
105715464 <105715464@student.swin.edu.au>
2025-11-01 22:27:16 +0700

    db change

--- project2/settings.php
+++ project2/settings.php
@@ -2,7 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pwd = "";
-$sql_db = "COS10026.3_projectdb";
+$sql_db = "projectpart2";
 $dbconn = mysqli_connect($host, $user, $pwd, $sql_db);
 if (!$dbconn) {
     die("Connection failed: " . mysqli_connect_error());
